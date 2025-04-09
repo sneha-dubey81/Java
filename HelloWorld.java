0f4b1fdbb20e762ba1f2729421183d07625200c1
sneha-dubey81 <sneha.dubey2005@gmail.com>
2025-04-09 11:15:36 +0530

    java class 1

---
+++ HelloWorld.java
@@ -0,0 +1,31 @@
+public class HelloWorld{
+
+    public static void main(String [] args){
+        System.out.println("Hello World!");
+         
+
+       /*byte a=20;
+       byte b=40;
+       int c =a+b; */
+
+       // boolean a= true;
+        //boolean b= false;
+        //System.out.println(a||b);
+
+       // int a=5;
+      //  int b=4;
+      //  System.out.println(a&b);  //bit operator
+
+     // System.out.println(5>6?"hii":"hello"); //if true - hii and if false- hello
+
+      boolean a= false;
+      if (a==true) {
+        System.out.println("true");
+    }
+    else {
+        System.out.println("false");
+    }
+
+    }
+}
+ 
---
+++ basic.java
@@ -0,0 +1,69 @@
+public class basic {
+    public static void main (String[] args){
+        /* 
+        int a=10;
+        int b=5;
+        System.out.println("Addition:"+(a+b));
+        System.out.println("Subtraction:"+(a-b));
+        System.out.println("Division:"+(a/b));
+        System.out.println("Multiplication:"+(a*b));
+
+        */
+
+      /*
+        //remainder using modulus
+
+        int a=159;
+        int b=5;
+        System.out.println("Remainder:"+(a%b));
+      */
+     
+      /* 
+      //Swap two numbers
+      int a=3; int b=6;
+      int temp=a;
+      a=b;
+      b=temp;
+      System.out.println("a:"+a + "b:"+b);
+      */
+
+
+      //swap without third variable
+
+
+
+    /*
+      //even or odd
+
+      int num=7;
+      if (num%2==0)
+      {
+        System.out.println("even");
+      }
+      else{
+        System.out.println("odd");
+    }
+    */
+    
+    /*
+     //positive,negative or zero
+
+     int num= -5;
+     if(num==0){
+        System.out.println("zero");
+     }
+        else if(num>0){
+            System.out.println("positive");
+        }
+        else {
+            System.out.println("negative");
+        }
+     
+    */
+
+
+
+
+    }
+    
+}
