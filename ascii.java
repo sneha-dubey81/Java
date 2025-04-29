4aae4c7a9fce9b7e6e2e8b83967a1da0a605673d
sneha-dubey81 <sneha.dubey2005@gmail.com>
2025-04-29 21:45:09 +0530

    ascii

--- ascii.java
+++ ascii.java
@@ -1,7 +1,12 @@
 public class ascii {
     static void abc(){
-
-    }
+        String[] str={"A","B","C","D","Z"};
+        for(int i=0;i<str.length;i++){
+            char ch=str[i].charAt(0);
+            int ascii=ch+32;
+            System.out.println((char)ascii);
+            }
+        }    
     public static void main(String[] args) {
         abc();
         
